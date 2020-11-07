02f99b54c0aae1faed861c6e74fee3acf4801c85
harjotscs <harjotscs@gmail.com>
2020-11-07 20:10:48 +0530

    updated app-header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
